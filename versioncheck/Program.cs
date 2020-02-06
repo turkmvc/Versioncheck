d8341d45327e43e8a5fe41261cc435640bf5ea30
Ahmet Doğukan Yıldıran <46098083+0x21@users.noreply.github.com>
2020-02-06 15:45:02 +0300

    Add files via upload

---
+++ versioncheck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace versioncheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://raw.githubusercontent.com/0x21/Versioncheck/master/version.txt");
+            string text = System.IO.File.ReadAllText(@"Version.txt");
+            if (text != reply)
+            {
+                Console.WriteLine("Yazılım güncel değil");
+                Console.WriteLine(reply);
+                Console.WriteLine(text);
+                
+            }
+            else
+            {
+                Console.WriteLine("Yazılımın Güncel");
+            }
+            Console.ReadKey();
+        }
+    }
+}
